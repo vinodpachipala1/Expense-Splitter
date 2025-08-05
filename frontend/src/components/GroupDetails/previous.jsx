5cb898017cb3f9b68651b0dbe61ab66ed14e192e
vinodpachipala1 <vinodpachipala93@gmail.com>
2025-08-05 18:18:22 +0530

    Fixed Errors

--- frontend/src/components/GroupDetails/previous.jsx
+++ frontend/src/components/GroupDetails/previous.jsx
@@ -59,7 +59,7 @@ const Previous =  () => {
                                 ))}
                                 <tr className="total-row">
                                     <th className="total-label">Total:</th>
-                                    <td className="total-amount">₹{expense.members_count}</td>
+                                    <td className="total-amount">₹{expense.members_count * expense.amount}</td>
                                 </tr>
                             </tbody>
                         </table>
